e5b2255c91b38ea64f3d69e932943940db098292
Juhyun Oh <ohjuhyun1@gmail.com>
2025-05-19 00:18:44 +0900

    2

--- yoroback/settings.py
+++ yoroback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lg*+*d5jo0nyw+z=z4=1ul+e%z@sqq@jwo%^25_u_8a4ydjs8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ohjuhyun1.pythonanywhere.com']
+ALLOWED_HOSTS = ['ohjuhyun1.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
